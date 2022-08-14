6839f379175fa9f08cf3c6f1596514e0b2311730
breef-droid <59095457+breef-droid@users.noreply.github.com>
2022-08-14 08:04:05 +0100

    Part 3

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+POSTS_LIST = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    },
+    
+]
 
 def home(request):
     '''
     Function to handle traffic from homepage of blog
     '''
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': POSTS_LIST
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     '''
     Function for about page landing
     '''
-    return HttpResponse('<h1>Blog About</h1>')
+    context = {
+        'title' : 'About Jou Ma'
+    }
+    return render(request, 'blog/about.html', context)
